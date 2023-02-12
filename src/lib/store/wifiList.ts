7fa199d9abdd9a057ba142dedd4a7066ef944877
Hung Duong Quang <qhungg289@protonmail.com>
2023-02-12 12:45:59 +0700

    add wifi list store

---
+++ src/lib/store/wifiList.ts
@@ -0,0 +1,21 @@
+import { writable, get } from "svelte/store";
+
+export type WifiInfo = {
+	ssid: string;
+	encryption: string;
+	password: string;
+	hidden: boolean;
+	dataURL: string;
+};
+
+export const wifiList = writable<WifiInfo[]>([]);
+
+export const addNewWifi = (wifi: WifiInfo) => {
+	wifiList.update((l) => [...l, wifi]);
+	localStorage.setItem("wifiList", JSON.stringify(get(wifiList)));
+};
+
+export const deleteWifi = (wifi: WifiInfo) => {
+	wifiList.update((l) => l.filter((w) => w !== wifi));
+	localStorage.setItem("wifiList", JSON.stringify(get(wifiList)));
+};
