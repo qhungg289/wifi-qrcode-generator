c79f0f70144d51e5c254b6f832e4b32e7c7eaa47
Hung Duong Quang <qhungg289@protonmail.com>
2023-02-12 12:46:15 +0700

    update +page.svelte

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,118 @@
+<script lang="ts">
+	import { page } from "$app/stores";
+	import "../app.css";
+</script>
+
+<svelte:head>
+	<title>WiFi2QR</title>
+	<meta name="description" content="Create a QR code with your WiFi information to share others." />
+</svelte:head>
+
+<header class="fixed top-0 inset-x-0 py-4 bg-zinc-800 border-b border-zinc-700">
+	<div class="container flex items-center justify-between">
+		<p class="text-lg">WiFi2QR</p>
+	</div>
+</header>
+
+<main>
+	<div class="container mb-32 mt-16">
+		<slot />
+	</div>
+</main>
+
+<nav
+	class="fixed bottom-0 inset-x-0 py-2 flex items-center justify-around bg-zinc-800 text-zinc-400 border-t border-zinc-700"
+>
+	<a
+		class="flex flex-col items-center gap-1 w-full"
+		class:text-blue-400={$page.url.pathname === "/"}
+		href="/"
+	>
+		{#if $page.url.pathname === "/"}
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				viewBox="0 0 24 24"
+				fill="currentColor"
+				class="w-6 h-6"
+			>
+				<path
+					fill-rule="evenodd"
+					d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zM12.75 9a.75.75 0 00-1.5 0v2.25H9a.75.75 0 000 1.5h2.25V15a.75.75 0 001.5 0v-2.25H15a.75.75 0 000-1.5h-2.25V9z"
+					clip-rule="evenodd"
+				/>
+			</svg>
+		{:else}
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				viewBox="0 0 24 24"
+				stroke-width="1.5"
+				stroke="currentColor"
+				class="w-6 h-6"
+			>
+				<path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+				/>
+			</svg>
+		{/if}
+		<span>Create</span>
+	</a>
+	<a
+		class="flex flex-col items-center gap-1 w-full"
+		class:text-blue-400={$page.url.pathname === "/saved"}
+		href="/saved"
+	>
+		{#if $page.url.pathname === "/saved"}
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				viewBox="0 0 24 24"
+				fill="currentColor"
+				class="w-6 h-6"
+			>
+				<path
+					fill-rule="evenodd"
+					d="M6.32 2.577a49.255 49.255 0 0111.36 0c1.497.174 2.57 1.46 2.57 2.93V21a.75.75 0 01-1.085.67L12 18.089l-7.165 3.583A.75.75 0 013.75 21V5.507c0-1.47 1.073-2.756 2.57-2.93z"
+					clip-rule="evenodd"
+				/>
+			</svg>
+		{:else}
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				viewBox="0 0 24 24"
+				stroke-width="1.5"
+				stroke="currentColor"
+				class="w-6 h-6"
+			>
+				<path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z"
+				/>
+			</svg>
+		{/if}
+		<span>Saved</span></a
+	>
+	<a
+		class="flex flex-col items-center gap-1 w-full"
+		class:text-blue-400={$page.url.pathname === "/search"}
+		href="/search"
+		><svg
+			xmlns="http://www.w3.org/2000/svg"
+			fill="none"
+			viewBox="0 0 24 24"
+			stroke-width="1.5"
+			stroke="currentColor"
+			class="w-6 h-6"
+		>
+			<path
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+			/>
+		</svg><span>Search</span>
+	</a>
+</nav>
+<div id="modal-container" />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,196 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import QRCode from "qrcode";
+	import Portal from "svelte-portal";
+	import { fade, fly } from "svelte/transition";
+	import { wifiList, addNewWifi } from "$lib/store/wifiList";
+	import { onMount } from "svelte";
+
+	let ssid = "";
+	let encryption: "WPA" | "WEP" | "nopass" = "WPA";
+	let password = "";
+	let hidden = false;
+	let qrSrc = "";
+
+	let isQrPreviewShow = false;
+
+	$: isPasswordFieldUnavailable = encryption === "nopass";
+
+	const formatToQR = () => {
+		const encryptionString = `T:${encryption};`;
+		const ssidString = `S:${ssid};`;
+		const paswordString = password ? `P:${password};` : "";
+		const hiddenString = hidden ? `H:${hidden};` : "";
+
+		const wifiInfoString = `WIFI:${encryptionString}${ssidString}${paswordString}${hiddenString};`;
+
+		QRCode.toDataURL(wifiInfoString, { scale: 50, margin: 3 }, (err, url) => {
+			if (err) console.error(err);
+
+			qrSrc = url;
+		});
+	};
+
+	const handleFormSubmit = () => {
+		formatToQR();
+
+		addNewWifi({ ssid, encryption, password, hidden, dataURL: qrSrc });
+	};
+
+	onMount(() => {
+		const savedFromLocal = localStorage.getItem("wifiList");
+		if (savedFromLocal) {
+			$wifiList = JSON.parse(savedFromLocal);
+		}
+	});
+</script>
+
+<hgroup class="py-8 space-y-4">
+	<h1 class="font-bold text-4xl">Create a new QR code</h1>
+	<h2 class="text-lg text-zinc-400">
+		Your WiFi information stays in your machine and did not send to any server.
+	</h2>
+</hgroup>
+
+<div class="bg-zinc-800 px-4 py-8 rounded-md border border-zinc-700">
+	<form on:submit|preventDefault={handleFormSubmit} class="space-y-4">
+		<label for="ssid" class="flex flex-col gap-1 text-zinc-300">
+			SSID
+			<input
+				bind:value={ssid}
+				type="text"
+				name="ssid"
+				id="ssid"
+				placeholder="Your WiFi name"
+				required
+				class="bg-zinc-900 rounded border-zinc-700 disabled:opacity-75 disabled:cursor-not-allowed"
+			/>
+		</label>
+
+		<fieldset class="flex flex-col gap-1">
+			<legend class="text-zinc-300">Encryption</legend>
+			<label for="wpa" class="flex items-center gap-2 text-zinc-300">
+				<input
+					bind:group={encryption}
+					type="radio"
+					name="wpa"
+					id="wpa"
+					value="WPA"
+					class="bg-zinc-900 border-zinc-700"
+				/>
+				WPA/WPA2/WPA3
+			</label>
+			<label for="wep" class="flex items-center gap-2 text-zinc-300">
+				<input
+					bind:group={encryption}
+					type="radio"
+					name="wep"
+					id="wep"
+					value="WEP"
+					class="bg-zinc-900 border-zinc-700"
+				/>
+				WEP
+			</label>
+			<label for="nopass" class="flex items-center gap-2 text-zinc-300">
+				<input
+					bind:group={encryption}
+					type="radio"
+					name="nopass"
+					id="nopass"
+					value="nopass"
+					class="bg-zinc-900 border-zinc-700"
+				/>
+				None
+			</label>
+		</fieldset>
+
+		<label for="password" class="flex flex-col gap-1 text-zinc-300">
+			Password
+			<input
+				bind:value={password}
+				disabled={isPasswordFieldUnavailable}
+				type="password"
+				name="password"
+				id="password"
+				placeholder="Your WiFi password"
+				required={encryption !== "nopass"}
+				class="bg-zinc-900 rounded border-zinc-700 disabled:opacity-75 disabled:cursor-not-allowed"
+			/>
+		</label>
+
+		<fieldset>
+			<label for="hidden" class="flex items-center gap-2 text-zinc-300">
+				<input
+					bind:checked={hidden}
+					type="checkbox"
+					name="hidden"
+					id="hidden"
+					class="bg-zinc-900 rounded border-zinc-700"
+				/>
+				Hidden
+			</label>
+		</fieldset>
+
+		<div class="flex justify-end gap-2">
+			{#if qrSrc}
+				<button
+					class="bg-zinc-700 rounded py-2 px-6 border border-zinc-600 hover:opacity-80 active:opacity-90 transition-all"
+					on:click|preventDefault={() => (isQrPreviewShow = !isQrPreviewShow)}
+					>{isQrPreviewShow ? "Hide QR code" : "Show QR code"}</button
+				>
+			{/if}
+			<button
+				type="submit"
+				class="bg-blue-600 border border-blue-500 hover:opacity-80 active:opacity-90 rounded px-6 py-2 transition-all"
+				>Generate</button
+			>
+		</div>
+	</form>
+</div>
+
+{#if isQrPreviewShow}
+	<Portal target="#modal-container">
+		<!-- svelte-ignore a11y-click-events-have-key-events -->
+		<div
+			class="fixed inset-0 flex items-center justify-center bg-zinc-900/80"
+			on:click={() => (isQrPreviewShow = false)}
+			in:fade
+			out:fade={{ delay: 100 }}
+		>
+			<div
+				on:click|stopPropagation
+				class="bg-zinc-800 rounded-md border border-zinc-700 p-4 flex flex-col items-center gap-8"
+				in:fly={{ delay: 100, y: -500 }}
+				out:fly={{ y: -500 }}
+			>
+				<div class="flex justify-end w-full">
+					<button
+						on:click={() => (isQrPreviewShow = false)}
+						class="p-2 rounded hover:bg-zinc-700 transition-all"
+						><svg
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							viewBox="0 0 24 24"
+							stroke-width="1.5"
+							stroke="currentColor"
+							class="w-6 h-6"
+						>
+							<path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+						</svg>
+					</button>
+				</div>
+				<img src={qrSrc} alt="qr code" class="max-w-[18rem] sm:max-w-xs rounded" />
+				<p
+					class="text-xl font-bold py-2 px-4 bg-zinc-700 border border-zinc-600 rounded-full truncate w-48 sm:w-60 md:w-72 text-center"
+				>
+					{ssid}
+				</p>
+				<a
+					download="wifi-qrcode.png"
+					class="bg-blue-600 border border-blue-500 hover:opacity-80 active:opacity-90 rounded px-6 py-2 transition-all w-full text-center"
+					role="button"
+					href={qrSrc}>Download</a
+				>
+			</div>
+		</div>
+	</Portal>
+{/if}
