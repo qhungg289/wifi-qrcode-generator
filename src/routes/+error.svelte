843ca1fe56ddad5e7b06b6c4caca346a4132470f
Hung Duong Quang <qhungg289@protonmail.com>
2023-02-12 13:02:58 +0700

    add error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import { page } from "$app/stores";
+</script>
+
+<hgroup class="py-8 space-y-4">
+	<h1 class="font-bold text-4xl">{$page.status}</h1>
+	<h2 class="text-lg text-zinc-400">{$page.error?.message}</h2>
+</hgroup>
