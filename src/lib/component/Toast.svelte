cd35c6dfe35f7b4955fe03e83d360c51b9ad7686
Hung Duong Quang <qhungg289@protonmail.com>
2023-02-16 21:13:39 +0700

    update Toast position

--- src/lib/component/Toast.svelte
+++ src/lib/component/Toast.svelte
@@ -2,7 +2,8 @@
 	import { onMount } from "svelte";
 	import { createEventDispatcher } from "svelte";
 	import Portal from "svelte-portal";
-	import { fade } from "svelte/transition";
+	import { quartInOut } from "svelte/easing";
+	import { fly } from "svelte/transition";
 
 	export let onScreenTime = 3000;
 
@@ -18,10 +19,11 @@
 </script>
 
 <Portal target="#modal-container">
-	<div
-		class="pointer-events-none fixed inset-0 h-screen flex flex-col items-center justify-end z-20"
-	>
-		<div transition:fade|local class="bg-zinc-800 px-6 py-2 mb-20 text-sm">
+	<div class="pointer-events-none fixed inset-0 flex flex-col items-center justify-start z-20">
+		<div
+			transition:fly|local={{ y: -100, easing: quartInOut }}
+			class="bg-zinc-800 px-6 py-4 mt-4 text-sm"
+		>
 			<slot />
 		</div>
 	</div>
