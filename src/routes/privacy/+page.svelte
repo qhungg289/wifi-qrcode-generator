fab04a2666c66eb914d51c753c59bedc26a8e2e5
Hung Duong Quang <qhungg289@protonmail.com>
2024-01-30 20:40:51 +0700

    add privacy page

---
+++ src/routes/privacy/+page.svelte
@@ -0,0 +1,112 @@
+<script lang="ts">
+	import type { PageData } from "./$types";
+
+	export let data: PageData;
+</script>
+
+<div class="prose prose-invert container">
+	<h1>Privacy Policy</h1>
+
+	<p>
+		This Privacy Policy governs the manner in which Wifi2QR collects, uses, maintains, and discloses
+		information collected from users (each, a "User") of the Wifi2QR application ("App"). This
+		privacy policy applies to the App and all products and services offered by [Your Company Name].
+	</p>
+
+	<h2>Information Collection and Use</h2>
+
+	<p>
+		We may collect personal identification information from Users in a variety of ways, including,
+		but not limited to, when Users visit our App, register on the App, fill out a form, respond to a
+		survey, and in connection with other activities, services, features, or resources we make
+		available on our App. Users may be asked for, as appropriate, name, email address, mailing
+		address, phone number. Users may, however, visit our App anonymously. We will collect personal
+		identification information from Users only if they voluntarily submit such information to us.
+		Users can always refuse to supply personally identification information, except that it may
+		prevent them from engaging in certain App-related activities.
+	</p>
+
+	<h2>Non-personal Identification Information</h2>
+
+	<p>
+		We may collect non-personal identification information about Users whenever they interact with
+		our App. Non-personal identification information may include the browser name, the type of
+		computer, and technical information about Users means of connection to our App, such as the
+		operating system and the Internet service providers utilized and other similar information.
+	</p>
+
+	<h2>How We Use Collected Information</h2>
+
+	<p>Wifi2QR may collect and use Users personal information for the following purposes:</p>
+	<ul>
+		<li>To personalize user experience</li>
+		<li>To improve our App</li>
+		<li>
+			To send periodic emails
+			<ul>
+				<li>
+					We may use the email address to respond to their inquiries, questions, and/or other
+					requests.
+				</li>
+				<li>
+					If User decides to opt-in to our mailing list, they will receive emails that may include
+					company news, updates, related product or service information, etc. If at any time the
+					User would like to unsubscribe from receiving future emails, we include detailed
+					unsubscribe instructions at the bottom of each email or User may contact us via our App.
+				</li>
+			</ul>
+		</li>
+	</ul>
+
+	<h2>How We Protect Your Information</h2>
+
+	<p>
+		We adopt appropriate data collection, storage, and processing practices and security measures to
+		protect against unauthorized access, alteration, disclosure, or destruction of your personal
+		information, username, password, transaction information, and data stored on our App.
+	</p>
+
+	<h2>Sharing Your Personal Information</h2>
+
+	<p>
+		We do not sell, trade, or rent Users personal identification information to others. We may share
+		generic aggregated demographic information not linked to any personal identification information
+		regarding visitors and users with our business partners, trusted affiliates, and advertisers for
+		the purposes outlined above.
+	</p>
+
+	<h2>Changes to This Privacy Policy</h2>
+
+	<p>
+		Wifi2QR has the discretion to update this privacy policy at any time. When we do, we will post a
+		notification on the main page of our App, revise the updated date at the bottom of this page. We
+		encourage Users to frequently check this page for any changes to stay informed about how we are
+		helping to protect the personal information we collect. You acknowledge and agree that it is
+		your responsibility to review this privacy policy periodically and become aware of
+		modifications.
+	</p>
+
+	<h2>Your Acceptance of These Terms</h2>
+
+	<p>
+		By using this App, you signify your acceptance of this policy. If you do not agree to this
+		policy, please do not use our App. Your continued use of the App following the posting of
+		changes to this policy will be deemed your acceptance of those changes.
+	</p>
+
+	<h2>Contacting Us</h2>
+
+	<p>
+		If you have any questions about this Privacy Policy, the practices of this App, or your dealings
+		with this App, please contact us at:
+	</p>
+
+	<p>
+		Wifi2QR<br />
+		Can Tho, Vietnam<br />
+		duongquanghung749@gmail.com<br />
+		+84877602933
+	</p>
+
+	<p>This document was last updated on 30/1/2024.</p>
+</div>
